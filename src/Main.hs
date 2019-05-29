c04579bd22ea64817265e5a4bcce5ac1758b3a75
Djoe Pramono <djoepramono@gmail.com>
2019-05-29 23:26:45 +1000

    Add simple command parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,23 @@
 module Main where
 
+data Input = Drink | Pay
+data Error
+data ParsedInput = Either Error Input
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Bartender: How can I help you?"
+  putStr "You: "
+  input <- getLine 
+  parseCommand (input)
+
+parseInput :: String -> Either Error Input -- Not sure why I can't use ParsedInput
+parseInput "drink" = Right Drink
+parseInput "pay" = Right Pay
+
+parseCommand :: String -> IO ()
+parseCommand command = do
+  case command of
+    "drink" -> putStrLn "Bartender: Here you go"
+    "pay" -> putStrLn "Bartender: Thanks"
+    _ -> putStrLn "Bartender: I don't get you"
