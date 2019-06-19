a9ecccf520b216421197d5811269ea5906cdd2d7
Djoe Pramono <djoepramono@gmail.com>
2019-06-19 23:08:31 +1000

    Almost there, just need to fix type inside do notation

--- src/Main.hs
+++ src/Main.hs
@@ -4,35 +4,36 @@ module Main where
 import Data.Either
 
 -- type alias
-type Drink = String
-type Pay = String
-type Unknown = String
+-- type Drink = String
+-- type Pay = String
+-- type Unknown = String
 
 data Request = Drink | Pay 
--- data Unknown
-data ParsedInput = Either Unknown Request
+data Error = ParsingError -- you cannot just put Error (type), you need ParsingError (value) as well. Right side of =
+data ParsedInput = Either Error Request
 
 main :: IO ()
 main = do
   putStrLn "Bartender: How can I help you?"
   putStr "You: "
   input <- getLine 
-  let request = parseCommand (input)
-  respond request
+  request <- parseRequest (input)
+  response <- respond (request)
+  putStrLn response
 
 parseRequest :: String -> ParsedInput -- Not sure why I can't use ParsedInput
 parseRequest "drink" = Right Drink
 parseRequest "pay" = Right Pay
-parseRequest _ = Left Unknown
+parseRequest _ = Left ParsingError 
 
-parseCommand :: String -> Request 
-parseCommand command = do
-  case command of
-    "drink" -> Right Drink  
-    "pay" -> Right Pay  
-    _ -> Left Unknown
+-- parseCommand :: String -> Request 
+-- parseCommand command = do
+--   case command of
+--     "drink" -> Right Drink  
+--     "pay" -> Right Pay  
+--     _ -> Left Unknown
 
 respond :: ParsedInput -> String 
 respond Right Drink  = "Bartender: Here you go"
 respond Right Pay  = "Bartender: Thanks"
-respond Left unknown = "Bartender: I don't get you"
+respond Left ParsingError = "Bartender: I don't get you"
