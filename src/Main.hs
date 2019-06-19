07318abc40435d3e29e9641f4de38ebc3c2f2537
Djoe Pramono <djoepramono@gmail.com>
2019-06-20 00:04:40 +1000

    add more comments after playing around more

--- src/Main.hs
+++ src/Main.hs
@@ -12,7 +12,10 @@ data Request = Drink | Pay
 data Error = ParsingError -- you cannot just put Error (type), you need ParsingError (value) as well. Right side of =
 -- data ParsedInput = Either Error Request -- wrong way apparently
 -- data Either Error Request = Left Error | Right Request
-type ParsedInput = Either Error Request
+type ParsedInput = Either Error Request 
+-- data ParsedInput = Either Error Request -- using data instead of type will blow up. Why? Not sure but more or less because Either is not a proper data constructor.
+-- data ParsedInput = Left Error | Right Request -- This is also not working, not sure why but basically it's ambiguous now as Left a | Right b is not Either a b
+
 
 main :: IO ()
 main = do
@@ -29,6 +32,7 @@ parseRequest "pay" = Right Pay
 parseRequest _ = Left ParsingError 
 
 respond :: ParsedInput -> String 
+-- respond :: Either Error Request -> String -- works as well
 respond (Right Drink)  = "Bartender: Here you go"
 respond (Right Pay)  = "Bartender: Thanks"
 respond (Left ParsingError) = "Bartender: I don't get you"
