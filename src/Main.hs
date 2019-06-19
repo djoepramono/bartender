c24be9d7af272467cfd00e6518cd94001b385523
Djoe Pramono <djoepramono@gmail.com>
2019-06-19 23:31:43 +1000

    works

--- src/Main.hs
+++ src/Main.hs
@@ -10,15 +10,17 @@ import Data.Either
 
 data Request = Drink | Pay 
 data Error = ParsingError -- you cannot just put Error (type), you need ParsingError (value) as well. Right side of =
-data ParsedInput = Either Error Request
+-- data ParsedInput = Either Error Request -- wrong way apparently
+-- data Either Error Request = Left Error | Right Request
+type ParsedInput = Either Error Request
 
 main :: IO ()
 main = do
   putStrLn "Bartender: How can I help you?"
   putStr "You: "
   input <- getLine 
-  request <- parseRequest (input)
-  response <- respond (request)
+  let request = parseRequest (input)
+  let response = respond (request)
   putStrLn response
 
 parseRequest :: String -> ParsedInput -- Not sure why I can't use ParsedInput
@@ -26,14 +28,7 @@ parseRequest "drink" = Right Drink
 parseRequest "pay" = Right Pay
 parseRequest _ = Left ParsingError 
 
--- parseCommand :: String -> Request 
--- parseCommand command = do
---   case command of
---     "drink" -> Right Drink  
---     "pay" -> Right Pay  
---     _ -> Left Unknown
-
 respond :: ParsedInput -> String 
-respond Right Drink  = "Bartender: Here you go"
-respond Right Pay  = "Bartender: Thanks"
-respond Left ParsingError = "Bartender: I don't get you"
+respond (Right Drink)  = "Bartender: Here you go"
+respond (Right Pay)  = "Bartender: Thanks"
+respond (Left ParsingError) = "Bartender: I don't get you"
